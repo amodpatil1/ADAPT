e69f35a257dc15819225b41360a5c5b1d5ee8e83
Ibrahim Al Dabbagh <ibr5456s@hs-coburg.de>
2024-01-31 09:45:47 +0100

    Update launch/adapt_launch.py

--- launch/adapt_launch.py
+++ launch/adapt_launch.py
@@ -47,7 +47,7 @@ def generate_launch_description():
             name= 'latlongcon'           
             
         )
-                Node(
+        Node(
             package='adapt_lanboun',           
             executable='lane_detection_node',   
             name= 'lane_detection'           
