364daff8663ed541811075e6f583edab6cc4f346
ha7sh <harshawardhan.patil@stud.hs-coburg.de>
2024-04-17 20:26:28 +0200

    updated launch file for demo

--- launch/demo_launch.py
+++ launch/demo_launch.py
@@ -4,10 +4,9 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
        Node(
-            package='adapt_transmitter',     
-            executable='transmitter_node',  
-            name= 'transmitter_data'          
-            
+            package='adapt_envmod',     
+            executable='map',  
+            name= 'mapping'              
         ),
        Node(
             package='adapt_loc',
@@ -16,9 +15,39 @@ def generate_launch_description():
         ),
         
         Node(
-            package='adapt_transmitter',     
-            executable='cpm',  
-            name= 'transmitternode' 
-        )    
+            package='adapt_transceiver',     
+            executable='transceiver_node',  
+            name= 'transceiver' 
+        ),
+        Node(
+            package='adapt_vi',     
+            executable='transceiver_node',  
+            name= 'transceiver' 
+        ) ,
+        Node(
+            package='adapt_mi',     
+            executable='transceiver_node',  
+            name= 'transceiver' 
+        ),
+        Node(
+            package='adapt_behplan',     
+            executable='beh_node',  
+            name= 'beh_plan' 
+        ),
+        Node(
+            package='adapt_roucomp',     
+            executable='route_node',  
+            name= 'route_comp' 
+        ),
+        Node(
+            package='adapt_livtrac',     
+            executable='livetrac_node',  
+            name= 'livetracker' 
+        ),
+        Node(
+            package='adapt_latloncon',     
+            executable='latlong',  
+            name= 'latlong_node' 
+        )                                 
             
     ])              
