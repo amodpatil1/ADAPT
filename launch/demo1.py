559d21700f8cba8cd8997e0a1353812ebfc5ee36
rid6804s <rid6804s@hs-coburg.de>
2024-05-01 15:12:47 +0200

    created launch files

---
+++ launch/demo1.py
@@ -0,0 +1,30 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+       
+        Node(
+            package='adapt_vi',     
+            executable='gvi_node',  
+            name= 'vi' 
+        ) ,
+        Node(
+            package='adapt_spotsl',
+            executable='spotsl_node',
+            name='spotsl'
+        ) ,
+        Node(
+            package='adapt_inf_spotupd',
+            executable='spot_upd6',
+            name='trial6'
+        ) ,
+        Node(
+            package='adapt_inf_trans',     
+            executable='inf_trans5',  
+            name= 'trial5' 
+        ) 
+
+                                
+            
+    ]) 
---
+++ launch/demo3.py
@@ -0,0 +1,22 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='adapt_behplan',     
+            executable='beh_node',  
+            name= 'beh_plan' 
+        ),
+        Node(
+            package='adapt_roucomp',     
+            executable='route_node',  
+            name= 'route_comp' 
+        ),
+        #Node(
+            #package='adapt_latloncon',     
+            #executable='latlong',  
+            #name= 'latlong_node' 
+        #)                                 
+            
+    ])              
