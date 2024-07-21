8023df1be1c57516fc7ee1a2fd03a2deaa34795c
rid6804s <rid6804s@hs-coburg.de>
2024-07-21 17:29:04 +0200

    created launch file for mvp

---
+++ launch/mvp.py
@@ -0,0 +1,58 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import IncludeLaunchDescription
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    # Find the share directory of the car_description package
+    car_description_share = FindPackageShare('car_description').find('car_description')
+
+    publish_model_launch_file = car_description_share + '/launch/publish_model.launch.py'
+
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(publish_model_launch_file)
+        ),
+
+        Node(
+            package='adapt_vi',     
+            executable='gvi_node',  
+            name='vi'
+        ),
+        Node(
+            package='adapt_spotsl',
+            executable='spotsl_node',
+            name='spotsl'
+        ),
+        Node(
+            package='adapt_loc',
+            executable='localization',
+            name='localization'
+        ),
+        Node(
+            package='adapt_roucomp',     
+            executable='route',  
+            name='routemodule5'
+        ),
+        Node(
+            package='adapt_trajp',
+            executable='traj',
+            name='trajectory_planner'
+        ),
+        Node(
+            package='adapt_envmod',
+            executable='env_mode',
+            name='envmod'
+        ),
+        Node(
+            package='adapt_envmod',
+            executable='map',
+            name='mapping'
+        ),
+        Node(
+            package='adapt_latlongcon',
+            executable='pp',
+            name='path_tracking'
+        )
+    ])
