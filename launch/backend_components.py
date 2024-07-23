0d031ea31d8c8a697c60350d607552f805773299
rid6804s <rid6804s@hs-coburg.de>
2024-07-24 00:06:57 +0200

    updated launch file

--- launch/backend_components.py
+++ launch/backend_components.py
@@ -1,45 +1,13 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.actions import IncludeLaunchDescription
-from launch_ros.substitutions import FindPackageShare
-from ament_index_python.packages import get_package_share_directory
-import os
+
 
 def generate_launch_description():
-    # Find the share directory of the car_description package
-    car_description_share = FindPackageShare('car_description').find('car_description')
-    
-    # Construct the path to the publish_model.launch.py file
-    publish_model_launch_file = os.path.join(car_description_share, 'launch', 'publish_model.launch.py')
-    
-    ros_deep_learning_share = FindPackageShare('ros_deep_learning').find('ros_deep_learning')
-    
-    # Construct the path to the detectnet.ros2.launch file
-    detectnet_launch_file = os.path.join(ros_deep_learning_share, 'launch', 'detectnet.ros2.launch')
-
-    # Find the share directory of the realsense2_camera package
-    realsense2_camera_share = FindPackageShare('realsense2_camera').find('realsense2_camera')
-    
-    # Construct the path to the rs_launch.py file
-    rs_launch_file = os.path.join(realsense2_camera_share, 'launch', 'rs_launch.py')
-
     return LaunchDescription([
-        # Include the publish_model.launch.py
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(publish_model_launch_file)
-        ),
-
-       
         Node(
-            package='adapt_loc',
-            executable='localization',
-            name='localization'
-        ),
-        Node(
-            package='adapt_roucomp',     
-            executable='route',  
-            name='routemodule5'
+            package='adapt_envmod',
+            executable='env_mod',
+            name='envmod'
         ),
         Node(
             package='adapt_trajp',
@@ -47,19 +15,9 @@ def generate_launch_description():
             name='trajectory_planner'
         ),
         Node(
-            package='adapt_envmod',
-            executable='env_mod',
-            name='envmod'
-        ),
-        Node(
-            package='adapt_latlongcon',
-            executable='pp',
-            name='path_tracking'
-        ),
-        Node(
-            package='adapt_behplan',
-            executable='behave',
-            name='beh'
+            package='adapt_loc',
+            executable='localization',
+            name='localization'
         ),
         Node(
             package='adapt_transceiver',
@@ -67,14 +25,10 @@ def generate_launch_description():
             name='transceiver'
         ),
         Node(
-            package='adapt_transceiver',
-            executable='cpm',
-            name='CPM'
-        ),
-        Node(
             package='ros2_pcan',
             executable='ros2pcan_node',
-            name='ros2_pcan'
+            name='ros2pcan'
         )
+
     ])
 
